84ad97f337540f72cc7d33a95f9df3b210973dc4
VitalikSokolov1920 <sokolov2048@mail.ru>
2023-05-12 14:23:28 +0300

    Добавил исполняемый файл утилиты

---
+++ exec/exec.sh
@@ -0,0 +1,14 @@
+# обязательный параметр
+export PGUSER='postgres'
+# обязательный параметр
+export PGPASSWORD='root'
+# обязательный параметр
+export PGDBNAME='test'
+# путь к файлу логирования(необязательный параметр)
+# значение по умолчанию: ./ivt_log.log
+export LOG_PATH='./IVT_log.log'
+# путь к директории для резервного копирования, слеш в конце ставить не нужно(необязательный параметр)
+# значение по умолчанию: ./backup
+export DUMP_PATH='./ivt_dump'
+
+./practice ./config.txt
